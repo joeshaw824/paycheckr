84299940faea8ac122f025fa42a0d1169d18954d
joeshaw824 <57156322+joeshaw824@users.noreply.github.com>
2024-10-06 08:50:31 +0100

    Add documentation

--- R/pay_pension.R
+++ R/pay_pension.R
@@ -1,4 +1,14 @@
+
 pay_pension <- function(monthly_pay, pension_rate = 0.098) {
+  #' Calculate the amount of pay put into your pension
+  #'
+  #' @param monthly_pay Monthly pay amount in pounds.
+  #' @param pension_rate The pension rate, which defaults to 9.8%.
+  #'
+  #' @return The amount to pay into your pension in pounds.
+  #' @export
+  #'
+  #' @examples pay_pension(3833)
 
   pension_amount <- monthly_pay * pension_rate
 
