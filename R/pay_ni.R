477e75829adaab254fe470d8584ee836ced4cb09
joeshaw824 <57156322+joeshaw824@users.noreply.github.com>
2024-10-06 10:27:29 +0100

    Add national insurance function

---
+++ R/pay_ni.R
@@ -0,0 +1,39 @@
+
+pay_ni <- function(weekly_pay,
+                   standard_bracket_lower = 242,
+                   standard_bracket_higher = 967,
+                   standard_ni_rate = 0.08,
+                   upper_ni_rate = 0.02){
+
+  #' Calculate national insurance payments
+  #'
+  #' The default values for this function are taken from the
+  #' [government website](https://www.gov.uk/national-insurance/how-much-you-pay).
+  #'
+  #' @param weekly_pay Weekly pay in pounds
+  #' @param standard_bracket_lower The lower pay threshold for the standard bracket
+  #' @param standard_bracket_higher The higher pay threshold for the standard bracket
+  #' @param standard_ni_rate The standard rate of national insurance
+  #' @param upper_ni_rate The upper rate of national insurance
+  #'
+  #' @return The amount of national insurance to pay weekly in pounds.
+  #' @export
+  #'
+  #' @examples pay_ni(350)
+
+  weekly_ni <- dplyr::case_when(
+    weekly_pay < standard_bracket_lower ~0,
+
+    (weekly_pay > standard_bracket_lower &
+       weekly_pay < standard_bracket_higher)
+
+    ~(weekly_pay - standard_bracket_lower) * standard_ni_rate,
+
+    weekly_pay > standard_bracket_higher ~((
+      standard_bracket_higher - standard_bracket_lower) *
+        standard_ni_rate) +
+      ((weekly_pay-standard_bracket_higher) * upper_ni_rate))
+
+  return(weekly_ni)
+
+}
